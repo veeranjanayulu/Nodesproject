3a2e0c5daeaafe9f07919e9a652af923524cbb50
hemanthkumarchintada <hemanthkumarchintada>
2024-11-11 11:28:40 +0530

    project

--- eks-terraform/backend.tf
+++ eks-terraform/backend.tf
@@ -7,9 +7,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "nareshawsdevv"
+    bucket = "panindiastarprabash"
     key    = "eks/terraform.tfstate"
-    region = "us-east-1"
+    region = "ca-central-1"
   }
 
   required_version = ">= 1.6.3"
