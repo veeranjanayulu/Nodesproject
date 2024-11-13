69a64226ce23a9a47fb9be5b591ae9bf33682941
hemanthkumarchintada <hemanthkumarchintada>
2024-11-13 17:08:21 +0530

    project

--- eks-terraform/main.tf
+++ eks-terraform/main.tf
@@ -106,7 +106,7 @@ provider "aws" {
 
   resource "aws_iam_instance_profile" "worker" {
     depends_on = [aws_iam_role.worker]
-    name       = "veera-eks-worker-new-profile1"
+    name       = "veera-eks-worker-new-profile11"
     role       = aws_iam_role.worker.name
   }
  
