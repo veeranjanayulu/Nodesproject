331cee542b64c3e613ea0230f26836b40ce6fea6
hemanthkumarchintada <hemanthkumarchintada>
2024-11-13 16:56:57 +0530

    project

--- eks-terraform/main.tf
+++ eks-terraform/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
  #Creating IAM role for EKS
   resource "aws_iam_role" "master" {
-    name = "veera-eks-master1"
+    name = "veera-eks-master11"
 
     assume_role_policy = jsonencode({
       "Version": "2012-10-17",
@@ -36,7 +36,7 @@ provider "aws" {
   }
 
   resource "aws_iam_role" "worker" {
-    name = "veera-eks-worker1"
+    name = "veera-eks-worker11"
 
     assume_role_policy = jsonencode({
       "Version": "2012-10-17",
